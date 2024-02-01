d1caff758a13cdaa03cc8f473e40de5afc856861
wasim2008 <158476928+wasim2008@users.noreply.github.com>
2024-02-01 18:48:59 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,43 @@
+const Orders = [
+    {
+        productName: 'TVS Dura Pro TL',
+        productNumber: '80.00.18',
+        productQuantity:'50',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+    {
+        productName: 'TVS Dura Pro TL',
+        productNumber: '3.00.18',
+        productQuantity:'15',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'TVS Dura Pro TT',
+        productNumber: '2.75.18',
+        productQuantity:'35',
+        paymentStatus: 'Due',
+        status: 'Panding'
+    },
+    {
+        productName: 'TVS Jumbo TT',
+        productNumber: '2.75.18',
+        productQuantity:'10',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+    {
+        productName: 'TVS Jumbo TT',
+        productNumber: '3.00.18',
+        productQuantity:'100',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+    {
+        productName: 'TVS jumbo TT',
+        productNumber: '3.00.17',
+        paymentStatus: 'Refunded',
+        status: 'Complete'
+    },
+]
